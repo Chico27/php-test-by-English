2cd355387837bd84a22a75a524bdfa9e11d52839
Chico27 <masamasa.987654321@gmail.com>
2019-05-16 22:04:55 +0900

    new commit

---
+++ www/test3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+      $phrase = "To be or not to be";
+      $age = 30;
+      $gpa = 30.3;
+      $isMale = false;
+      echo "$phrase <br>";
+      echo "$age <br>";
+      echo "$gpa <br>";
+      echo "$isMale <br>";
+
+     ?>
+
+  </body>
+</html>
---
+++ www/test4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+      $phrase = "Giraffe Academy";
+      echo mb_strtolower("$phrase <br>");
+      echo mb_strtoupper("$phrase <br>");
+      echo mb_strlen($phrase);
+      echo "<br>";
+      echo "$phrase[0] <br>";
+     ?>
+
+  </body>
+</html>
